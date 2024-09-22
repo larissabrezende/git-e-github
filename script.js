b14bfad3cb70db87e55f7a256e71f850de663ae6
larissabrezende <larissa.abrezende@gmail.com>
2024-09-22 14:43:49 -0300

    Adiciona funcionalidade para entrada via teclado

--- script.js
+++ script.js
@@ -23,4 +23,18 @@ document.querySelectorAll(".charKey").forEach(function (charKeyBtn) {
       copyButton.classList.remove("success");
     }
   });
+
+  input.addEventListener("keydown", function (ev) {
+    ev.preventDefault();
+    if (allowedKeys.includes(ev.key)) {
+      input.value += ev.key;
+      return;
+    }
+    if (ev.key === "Backspace") {
+      input.value = input.value.slice(0, -1);
+    }
+    if (ev.key === "Enter") {
+      calculate();
+    }
+  });
   
